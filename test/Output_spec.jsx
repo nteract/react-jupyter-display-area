f5ce365b1dea5017ed65c396ecb31863e7667bd4
Kyle Kelley <rgbkrk@gmail.com>
2016-03-14 22:36:12 -0500

    Handle error display printout

--- test/Output_spec.jsx
+++ test/Output_spec.jsx
@@ -19,11 +19,11 @@ describe('Output', () => {
     const renderer = createRenderer();
 
     const output = Immutable.fromJS({
+      output_type: 'display_data',
       data:
           { 'text/html': '<h1>Multiple</h1>',
             'text/plain': '<IPython.core.display.HTML object>' },
       metadata: {},
-      output_type: 'display_data',
     });
 
     renderer.render(<Output output={output} />);
@@ -34,13 +34,43 @@ describe('Output', () => {
     const renderer = createRenderer();
 
     const output = Immutable.fromJS({
-      name: 'stdout',
       output_type: 'stream',
+      name: 'stdout',
       text: 'hey\n',
     });
 
     renderer.render(<Output output={output} />);
     const result = renderer.getRenderOutput();
     expect(result).to.deep.equal(<ConsoleText text={'hey\n'} />);
+  });
+  it('handles errors/tracebacks', () => {
+    const renderer = createRenderer();
+
+    const output = Immutable.fromJS({
+      output_type: 'error',
+      traceback: 'whoa there buckaroo!',
+      ename: 'BuckarooException',
+      evalue: 'whoa!',
+    });
+
+    renderer.render(<Output output={output} />);
+    const result = renderer.getRenderOutput();
+
+    expect(result).to.deep.equal(<ConsoleText text={output.get('traceback').join('\n')} />);
+
+    const outputNoTraceback = Immutable.fromJS({
+      output_type: 'error',
+      ename: 'BuckarooException',
+      evalue: 'whoa!',
+    });
+
+    renderer.render(<Output output={output} />);
+    const result2 = renderer.getRenderOutput();
+
+    expect(result2).to.deep.equal(
+      <ConsoleText
+        text={`${outputNoTraceback.get('ename')}: ${outputNoTraceback.get('evalue')}`}
+      />
+    );
   });
 });
